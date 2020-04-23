0afdeddef6891caa2250ddca305af13d446c9d6f
Sara Al-Jammal <sara.aljammal97@gmail.com>
2020-04-23 17:23:00 +0300

    added hairColor

---
+++ src/PersonTest.java
@@ -0,0 +1,8 @@
+public class PersonTest {
+    public static void main(String[] args){
+
+        Person p1 = new Person("Sara", 20, "Black");
+        System.out.println(p1.getHaircolor());
+
+    }
+}
