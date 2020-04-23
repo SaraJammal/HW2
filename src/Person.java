24b8dacaaf7f4cc4f976f6e839562bb3c6382e25
Sara Al-Jammal <sara.aljammal97@gmail.com>
2020-04-23 17:07:41 +0300

    added haircolor

--- src/Person.java
+++ src/Person.java
@@ -2,14 +2,16 @@ public class Person {
 
     private String name;
     private int age;
+    private String haircolor;
 
     public Person(){
         name = "";
         age = 0;
     }
-    public Person(String name, int age){
+    public Person(String name, int age, String haircolor){
         this.name = name;
         this.age = age;
+        this.haircolor = haircolor;
     }
 
     public void setAge(int age) {
@@ -20,11 +22,19 @@ public class Person {
         this.name = name;
     }
 
+    public void setHaircolor(String haircolor) {
+        this.haircolor = haircolor;
+    }
+
     public int getAge() {
         return age;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getHaircolor() {
+        return haircolor;
     }
 }
